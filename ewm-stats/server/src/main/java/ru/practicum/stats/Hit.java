de68f3773a1db7c885253ee08c8e0a33cd8017b8
hlypik <arsenyborovskoi@yandex.ru>
2025-09-09 17:24:56 +0300

    Добавлена возможность работы с БД

---
+++ ewm-stats/server/src/main/java/ru/practicum/stats/Hit.java
@@ -0,0 +1,24 @@
+package ru.practicum.stats;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hits")
+public class Hit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String app;
+    private String uri;
+    private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
---
+++ ewm-stats/server/src/main/java/ru/practicum/stats/StatsRepository.java
@@ -0,0 +1,22 @@
+package ru.practicum.stats;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface StatsRepository extends JpaRepository<Hit, Long> {
+
+    @Query("SELECT new ru.practicum.stats.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
+    " FROM Hit h " +
+    " WHERE uri IN :uris AND (timestamp >= :start AND timestamp < :end) GROUP BY h.app, h.uri")
+    List<ViewStats> calculateUniqueStats(List<String> uris, LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT new ru.practicum.stats.ViewStats(h.app, h.uri, COUNT(h.ip)) " +
+    " FROM Hit h " +
+    " WHERE uri IN :uris AND (timestamp >= :start AND timestamp < :end) GROUP BY h.app, h.uri")
+    List<ViewStats> calculateStats(List<String> uris, LocalDateTime start, LocalDateTime end);
+}
