b061176df27dd229b556d2a9188728eda62776f4
sh-ilnar <ishaikhutdinov@yandex.ru>
2025-09-24 09:57:30 +0300

    убрал лишние импорты

--- ewm-service/src/main/java/ru/practicum/request/RequestServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/request/RequestServiceImpl.java
@@ -2,12 +2,9 @@ package ru.practicum.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.common.exception.ConflictException;
 import ru.practicum.common.exception.NotFoundException;
@@ -274,7 +271,7 @@ public class RequestServiceImpl implements RequestService {
         return response;
     }
 
-    private void validateLimit (Event event, RequestsChangeStatusRequestDto dto) throws ConflictException {
+    private void validateLimit(Event event, RequestsChangeStatusRequestDto dto) throws ConflictException {
         int confirmedRequestsCount = requestRepository.countByEventIdAndStatus(event.getId(), RequestStatus.CONFIRMED);
         int availableSlotsCount = event.getParticipantLimit() - confirmedRequestsCount;
         if (availableSlotsCount < dto.getRequestIds().size())
