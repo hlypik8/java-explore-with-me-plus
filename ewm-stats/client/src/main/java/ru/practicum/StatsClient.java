abacf3d5b8253038883ad05eb774ceb20166495c
hlypik <arsenyborovskoi@yandex.ru>
2025-09-27 13:11:20 +0300

    Правки кодстайла

--- ewm-stats/client/src/main/java/ru/practicum/StatsClient.java
+++ ewm-stats/client/src/main/java/ru/practicum/StatsClient.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
