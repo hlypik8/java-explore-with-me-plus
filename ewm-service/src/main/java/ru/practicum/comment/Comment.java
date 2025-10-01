c13ef1f780fb78910f21b75448aa8be614d6e6f6
Rustam Aliev <azerich@yandex.ru>
2025-10-01 14:17:42 +0300

    Реализовал private эндпойнты в соответствии с заданием

---
+++ ewm-service/src/main/java/ru/practicum/comment/Comment.java
@@ -0,0 +1,63 @@
+package ru.practicum.comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.event.Event;
+import ru.practicum.user.User;
+
+/**
+ * Комментарий.
+ */
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Comment {
+
+    /**
+     * Идентифиептор
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    /**
+     * Автор
+     */
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    /**
+     * Комментируемое событие
+     */
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    /**
+     * Содержимое
+     */
+    @Column(name = "text")
+    private String text;
+
+    /**
+     * Дата создания
+     */
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+}
---
+++ ewm-service/src/main/java/ru/practicum/comment/CommentMapper.java
@@ -0,0 +1,41 @@
+package ru.practicum.comment;
+
+import java.time.format.DateTimeFormatter;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import ru.practicum.comment.dto.CommentCreateOrUpdateDto;
+import ru.practicum.comment.dto.CommentFullDto;
+import ru.practicum.event.EventMapper;
+import ru.practicum.user.UserMapper;
+
+@UtilityClass
+@Slf4j
+public class CommentMapper {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public Comment mapToComment(CommentCreateOrUpdateDto dto) {
+        log.info("Преобразование модели {} в модель {} для сохранения", CommentCreateOrUpdateDto.class, Comment.class);
+        return Comment.builder()
+                .text(dto.getText())
+                .build();
+    }
+
+    public CommentFullDto mapToCommentFullDto(Comment comment) {
+        log.info("Преобразование модели {} в модель {}", Comment.class, CommentFullDto.class);
+        return CommentFullDto.builder()
+                .id(comment.getId())
+                .author(UserMapper.mapToUserShortDto(comment.getAuthor()))
+                .event(EventMapper.mapToEventShortDto(comment.getEvent()))
+                .text(comment.getText())
+                .created(comment.getCreatedOn().format(DATE_TIME_FORMATTER))
+                .build();
+    }
+
+    public void updateFields(Comment comment, CommentCreateOrUpdateDto dto) {
+        log.info("Преобразование модели {} в модель {} для обновления", CommentCreateOrUpdateDto.class, Comment.class);
+        if (!(dto.getText() == null || dto.getText().trim().isBlank())) {
+            comment.setText(dto.getText());
+        }
+    }
+}
